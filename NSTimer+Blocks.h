fe8c32a671c37103b31194adf612e9e9593a4132
Jesse Bunch <jesse@getbunch.com>
2011-12-29 13:22:19 -0600

    Added ARC Compatibility

--- NSTimer+Blocks.h
+++ NSTimer+Blocks.h
@@ -7,6 +7,20 @@
 
 #import <Foundation/Foundation.h>
 
+#ifndef NSTB_USE_ARC
+#define NSTB_USE_ARC __has_feature(objc_arc)
+#endif
+
+#if NSTB_USE_ARC
+#define NSTB_RETAIN(xx)
+#define NSTB_RELEASE(xx)
+#define NSTB_AUTORELEASE(xx)
+#else
+#define NSTB_RETAIN(xx)           [xx retain];
+#define NSTB_RELEASE(xx)          [xx release];
+#define NSTB_AUTORELEASE(xx)      [xx autorelease];
+#endif
+
 @interface NSTimer (Blocks)
 +(id)scheduledTimerWithTimeInterval:(NSTimeInterval)inTimeInterval block:(void (^)())inBlock repeats:(BOOL)inRepeats;
 +(id)timerWithTimeInterval:(NSTimeInterval)inTimeInterval block:(void (^)())inBlock repeats:(BOOL)inRepeats;
--- NSTimer+Blocks.m
+++ NSTimer+Blocks.m
@@ -13,7 +13,7 @@
 {
     void (^block)() = [inBlock copy];
     id ret = [self scheduledTimerWithTimeInterval:inTimeInterval target:self selector:@selector(jdExecuteSimpleBlock:) userInfo:block repeats:inRepeats];
-    [block release];
+    NSTB_RELEASE(block);
     return ret;
 }
 
@@ -21,7 +21,7 @@
 {
     void (^block)() = [inBlock copy];
     id ret = [self timerWithTimeInterval:inTimeInterval target:self selector:@selector(jdExecuteSimpleBlock:) userInfo:block repeats:inRepeats];
-    [block release];
+    NSTB_RELEASE(block);
     return ret;
 }
 
